cd586551b9ac954bfa9d9248b40f8a12a158c546
isayde <63158781+isayde@users.noreply.github.com>
2023-09-01 08:30:35 -0300

    Add files via upload

---
+++ propriedades.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int numero;
+    float resto;
+    printf("Digite um número inteiro positivo:\n");
+    scanf("%d", &numero);
+    system("clear");
+    if(numero<0){
+        printf("O número digitado deve ser positivo, execute o programa novamente\n");
+        return 1;
+    } else {
+        resto = numero % 3;
+        printf("O número digitado foi %d\n", numero);
+        printf("------------------------------------------------\n");
+        printf("1) O número digitado ao quadrado é: %d\n", numero*numero);
+        printf("2) O número digitado ao cubo é: %d\n", numero*numero*numero);
+        printf("3) A raiz quadrada do número digitado é: %.2f\n", sqrt(numero));
+        printf("4) A raiz cúbica do número digitado é: %.2f\n", pow(numero,(1.0/3.0)));
+        printf("5) O resto da divisão do número por 3 é: %.2f\n", resto);
+        
+    }
+}
